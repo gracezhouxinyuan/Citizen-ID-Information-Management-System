e78d69899eb10a46f919160f3466bdc4ffa3b563
XINYUAN ZHOU <127930807+gracezhouxinyuan@users.noreply.github.com>
2026-05-17 14:21:24 +0800

    Refactor ID.c for improved code organization
    Enhanced the structure and functionality of ID management system.

--- ID.c
+++ ID.c
@@ -52,7 +52,21 @@ void birth_sort(struct person p[], int n)
 
 int search(struct person p[], int n, char *name)
 {
-
+    int i;
+    int cnt=0;
+    printf("\n同名且身份证校验正确的信息如下:\n");
+    printf("%-20s %-12s %-12s\n", "ID", "Name", "Birthday");
+    printf("----------------------------------------------------\n");
+    for (i=0;i<n;i++)
+    {
+        if (p[i].flag==1&&strcmp(p[i].name, name)==0)
+        {
+            printf("%-20s %-12s %04d-%02d-%02d\n",p[i].ID, p[i].name,p[i].birthday.year, p[i].birthday.month, p[i].birthday.day);
+            cnt++;
+        }
+    }
+    printf("\n名为%s的人有%d个\n", name, cnt);
+    return cnt;
 }
 
 void save(struct person p[], int n)
@@ -79,9 +93,42 @@ int main(void)
 {
     struct person p[N];
     int cmd, n;
+    char*name;
+    name=(char*)malloc(20*sizeof(char));
+    n=read(p);
+    get_all_person(p, n);
+    save(p, n);
     while (1)
-    {   cmd = prompt();
-
+    {   cmd=prompt();
+        if (cmd==0)
+        {
+            printf("程序结束。\n");
+            break;
+        }
+        if (cmd==1)
+        {
+            display_person(p, n, 1);
+        }
+        else if (cmd==2)
+        {
+            display_person(p, n, 0);
+        }
+        else if (cmd==3)
+        {
+            birth_sort(p, n);
+            display_person(p, n, 1);
+        }
+        else if (cmd==4)
+        {
+            printf("请输入要查询的姓名:");
+            scanf("%19s", name);
+            search(p, n, name);
+        }
+        else
+        {
+            printf("输入无效，请重新输入。\n");
+        }
     } 
+    free(name);
     return 0;
 }
